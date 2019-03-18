71cc1e9d405e523372261cbe9d872a1c9a0fe539
Saket Kumar Choudhary <saket_au@rediffmail.com>
2019-03-18 15:33:22 +0530

    update index.php with header and footer

--- project/index.php
+++ project/index.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <title>Simple Database App</title>
-
-    <link rel="stylesheet" href="css/style.css" />
-  </head>
-
-  <body>
-    <h1>Simple Database App</h1>
-
+<?php include "templates/header.php"; ?>
     <ul>
       <li>
         <a href="create.php"><strong>Create</strong></a> - add a user
@@ -21,5 +7,4 @@
         <a href="read.php"><strong>Read</strong></a> - find a user
       </li>
     </ul>
-  </body>
-</html>
+<?php include "templates/footer.php"; ?>

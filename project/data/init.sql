fb0c9f85263b8fd8f415c09f43c8e31a79617dd4
Saket Kumar Choudhary <saket_au@rediffmail.com>
2019-03-18 16:18:50 +0530

    add config file, init query and create table

---
+++ project/data/init.sql
@@ -0,0 +1,12 @@
+
+  use test;
+
+  CREATE TABLE users (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    age INT(3),
+    course VARCHAR(50),
+    date TIMESTAMP
+  );

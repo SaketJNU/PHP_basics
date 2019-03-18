dfba0ba6929724e83fbc073357b72460fadb2aad
Saket Kumar Choudhary <saket_au@rediffmail.com>
2019-03-18 10:12:41 +0530

    insert item in database

---
+++ code/insert_item.php
@@ -0,0 +1,25 @@
+e (MySQLi Object-oriented)
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Saket', 'Kumar', 'abc@xyz')";
+
+if ($conn->query($sql) === TRUE) {
+echo "New record created successfully";
+} else {
+echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>

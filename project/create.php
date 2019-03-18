a30cd9074b94c660097c8dc3f6e33a300974a747
Saket Kumar Choudhary <saket_au@rediffmail.com>
2019-03-18 15:36:26 +0530

    add create form

---
+++ project/create.php
@@ -0,0 +1,19 @@
+<?php include "templates/header.php"; ?><h2>Add a user</h2>
+
+    <form method="post">
+    	<label for="firstname">First Name</label>
+    	<input type="text" name="firstname" id="firstname">
+    	<label for="lastname">Last Name</label>
+    	<input type="text" name="lastname" id="lastname">
+    	<label for="email">Email Address</label>
+    	<input type="text" name="email" id="email">
+    	<label for="age">Age</label>
+    	<input type="text" name="age" id="age">
+    	<label for="course">Course</label>
+    	<input type="text" name="course" id="course">
+    	<input type="submit" name="submit" value="Submit">
+    </form>
+
+    <a href="index.php">Back to home</a>
+
+    <?php include "templates/footer.php"; ?>

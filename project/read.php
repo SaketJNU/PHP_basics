a9b41d6011dc04f78f42b1cc49104eef51242d83
Saket Kumar Choudhary <saket_au@rediffmail.com>
2019-03-20 07:00:50 +0530

    code to read data

--- project/read.php
+++ project/read.php
@@ -1,13 +1,80 @@
-<?php include "templates/header.php"; ?>
+<?php
 
-    <h2>Find user based on Course</h2>
+/**
+  * Function to query information based on
+  * a parameter: in this case, course.
+  *
+  */
 
-    <form method="post">
-    	<label for="course">Course</label>
-    	<input type="text" id="course" name="course">
-    	<input type="submit" name="submit" value="View Results">
-    </form>
+if (isset($_POST['submit'])) {
+  try {
+  echo $_POST['course'];
+    require "../project/config.php";
+    require "../project/common.php";
 
-    <a href="index.php">Back to home</a>
+    $connection = new PDO($dsn, $username, $password, $options);
 
-    <?php include "templates/footer.php"; ?>
+    $sql = "SELECT *
+    FROM users
+    WHERE course = :course";
+
+    $course = $_POST['course'];
+    $statement = $connection->prepare($sql);
+    $statement->bindParam(':course', $course, PDO::PARAM_STR);
+    $statement->execute();
+
+    $result = $statement->fetchAll();
+  } catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+  }
+}
+?>
+<?php require "templates/header.php"; ?>
+
+<?php
+if (isset($_POST['submit'])) {
+  if ($result && $statement->rowCount() > 0) { ?>
+    <h2>Results</h2>
+
+    <table>
+      <thead>
+<tr>
+  <th>#</th>
+  <th>First Name</th>
+  <th>Last Name</th>
+  <th>Email Address</th>
+  <th>Age</th>
+  <th>Course</th>
+  <th>Date</th>
+</tr>
+      </thead>
+      <tbody>
+  <?php foreach ($result as $row) { ?>
+      <tr>
+<td><?php echo escape($row["id"]); ?></td>
+<td><?php echo escape($row["firstname"]); ?></td>
+<td><?php echo escape($row["lastname"]); ?></td>
+<td><?php echo escape($row["email"]); ?></td>
+<td><?php echo escape($row["age"]); ?></td>
+<td><?php echo escape($row["course"]); ?></td>
+<td><?php echo escape($row["date"]); ?> </td>
+      </tr>
+    <?php } ?>
+      </tbody>
+  </table>
+  <?php } else { ?>
+    > No results found for <?php echo escape($_POST['course']); ?>
+  <?php }
+} ?>
+
+<h2>Find user based on course</h2>
+
+<form method="post">
+  <label for="course">Course</label>
+  <input type="text" id="course" name="course">
+  <input type="submit" name="submit" value="View Results">
+</form>
+
+<a href="index.php">Back to home</a>
+
+<?php require "templates/footer.php"; ?>
